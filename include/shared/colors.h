a3d1cdd9bb914a7f8307d759d73c235ed1986886
Ivan <capobianco.ivan@gmail.com>
2021-05-27 18:19:02 +0200

    refactor: move colors define to separate file

---
+++ include/shared/colors.h
@@ -0,0 +1,10 @@
+#ifndef SORTING_SHARED_COLORS_H_
+#define SORTING_SHARED_COLORS_H_
+
+#define YELLOW CRGB(0xeaf70c)
+#define GREEN CRGB(0xf70c23)
+#define RED CRGB(0x44f70c)
+#define BLUE CRGB(0x0c30f7)
+#define NO_COLOR CRGB(0x000000)
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,12 @@
 #include <FastLED.h>
 
 #include "utils/utils.h"
+#include "shared/colors.h"
 
 #define LED_D1 2
 #define NUM_LEDS 12
 
-#define YELLOW CRGB(0xeaf70c)
-#define GREEN CRGB(0xf70c23)
-#define RED CRGB(0x44f70c)
-#define BLUE CRGB(0x0c30f7)
-#define NO_COLOR CRGB(0x000000)
-
-#define DELAY_BY 300
+#define DELAY_BY 600
 
 CRGB leds[NUM_LEDS];
 
